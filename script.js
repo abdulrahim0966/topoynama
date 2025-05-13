89c37d29a251505ec1f51ea13f1b15a6e7b020f7
abdulrahim0966 <abok0965@gmail.com>
2025-05-13 21:07:42 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const yazi = document.getElementById('yazi');
+
+  yazi.addEventListener('mouseover', function () {
+    alert('Nasılsın');
+  });
+});
